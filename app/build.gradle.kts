e53426e1522145f07ff7b8dfd02c34dcff7f6756
Yu-ga <74749461+yuga-hashimoto@users.noreply.github.com>
2026-07-20 13:31:53 +0900

    chore: bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.opencode.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
